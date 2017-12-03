58c87011c937746e4574caeea0f943b8e8165ead
Blabel <blakecabel@gmail.com>
2017-12-03 00:33:05 -0600

    Cleaned up directory names a bit (if sed was nice that would help a lot for subdirectories...

--- install.zsh
+++ install.zsh
@@ -2,13 +2,12 @@
 
 # Variables
 
-dir="/home/blabel/.dotfiles"
-subdir="/interface"
+dir=~/.dotfiles/
+subdir=./interface
 
-# Create symlinks
-echo $dir$subdir
+# Create symlinks in config for specified subdirectories
 
-for file in $(find -H .$subdir -maxdepth 2 -type f | sed 's|./interface||'); do
+for file in $(find -H $subdir -maxdepth 2 -type f | sed 's|./interface||'); do
 	echo "Creating symlink to $file."
 	echo $dir$subdir$file ~/.config$file
 	ln -sf $dir$subdir$file ~/.config$file
