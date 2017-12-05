0515d9ae16e30fc7163ba5084ba957132bd7e55e
Blabel <blakecabel@gmail.com>
2017-12-05 00:09:09 -0600

    Got multiple subdirectories working and added in graphics.

--- install.zsh
+++ install.zsh
@@ -3,13 +3,19 @@
 # Variables
 
 dir=~/dotfiles/
-subdir="./interface"
+declare -a subdirs=("./interface" "./graphics")
 
 # Create symlinks in config for specified subdirectories
 
-for file in $(find -H $subdir -maxdepth 2 -type f | sed "s|$subdir||"); do
-	echo "Creating symlink to $file."
-	echo $dir$subdir$file --- ~/.config$file
-	ln -sf $dir$subdir$file ~/.config$file
+
+for subdir in ${subdirs[@]}; do
+
+	for file in $(find -H $subdir -maxdepth 2 -type f | sed "s|$subdir||"); do
+		echo "Creating symlink in ~/.config to $file ..."
+		# echo $dir$subdir$file --- ~/.config$file
+		ln -sf $dir$subdir$file ~/.config$file
+	done
+
 done
 
+echo "Done!"
